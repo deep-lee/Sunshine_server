af158c1a58e723d643bfc4a44b3caae5ac72d19e
李冬 <1136855714@qq.com>
2016-06-15 22:17:04 +0800

    更新

--- withdraw_self_by_row.php
+++ withdraw_self_by_row.php
@@ -5,18 +5,32 @@
  * Date: 6/7/16
  * Time: 4:49 PM
  */
+
 /*
  * 分页加载提款记录
  */
+
 include 'conn.php';
 include 'My.php';
-if (isset($_POST["lastId"]) || isset($_POST["project_id"])) {
-    $lastId = $_POST["lastId"];
-    $project_id = $_POST["project_id"];
+
+$per_page = 10;
+
+if (isset($_POST["user_id"]) || isset($_POST["row"])) {
+
+    $user_id = $_POST["user_id"];
+    $row = $_POST["row"];
+
     $sql = "
-    SELECT * FROM User, Withdraw WHERE User.id = Withdraw.user_id AND Withdraw.project_id = $project_id AND Withdraw.id < $lastId ORDER BY Withdraw.create_time DESC LIMIT 10
+        SELECT *
+        FROM Withdraw, Project
+        WHERE Withdraw.user_id = $user_id AND Withdraw.project_id = Project.id AND Project.launcher_id = $user_id
+        ORDER BY Withdraw.create_time
+        DESC
+        LIMIT $row, $per_page
     ";
+
     $result = mysql_query($sql);
+
     if ($result == false) {
         MyError(101, 201);
     }
@@ -25,7 +39,9 @@ if (isset($_POST["lastId"]) || isset($_POST["project_id"])) {
     while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
         $arr[$n++] = $row;
     }
+
     MySuccess($arr, 200);
+
 } else {
     MyError(101, 201);
 }
